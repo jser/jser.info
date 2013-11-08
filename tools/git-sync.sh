c93ab6af0d29005749b1f0e08f87959d8a8b3b7c
azu <azuciao@gmail.com>
2013-11-09 01:35:00 +0900

    update

--- tools/git-sync.sh
+++ tools/git-sync.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 declare parentDir=$(cd $(dirname $(cd $(dirname $0);pwd));pwd)
+cd $(cd $(dirname $(cd $(dirname $0);pwd));pwd)
 git pull
-git add -A "${parentDir}/"
+git add -A .
 git commit -m "$1"
 git push
+
