091cbe09e6c563054f36be555d67aca13ea33b95
azu <azuciao@gmail.com>
2013-12-08 21:31:31 +0900

    Add: Hyperglot · A Language Experimentation Platform

--- tools/git-sync.sh
+++ tools/git-sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 declare parentDir=$(cd $(dirname $(cd $(dirname $0);pwd));pwd)
 cd $(cd $(dirname $(cd $(dirname $0);pwd));pwd)
-git pull
+git pull origin gh-pages
 git add -A .
 git commit -m "$1"
 git push
