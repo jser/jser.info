8d8910a745acea406ae74a0a1b3a6d07bc2889cf
azu <azuciao@gmail.com>
2014-01-07 23:26:57 +0900

    fix data path

--- js/edit-on-github.js
+++ js/edit-on-github.js
@@ -4,13 +4,12 @@
 $(function () {
     // yyyy-MM-dd に該当する付きのjsonを開く
     function editOnGithub(dateString) {
-        var editURL = "https://github.com/azu/jser.info/edit/gh-pages/data/" + dateString + "/index.json";
+        var editURL = "https://github.com/azu/jser.info/edit/gh-pages/" + dateString + "/index.json";
         location.href = editURL;
     }
 
     var $edit = $("#edit-on-github");
     $edit.button().on("click", function (event) {
-        console.log("event", event);
         var dirPath = $("#content").data("file-dir-path");
         if (dirPath) {
             editOnGithub(dirPath);
