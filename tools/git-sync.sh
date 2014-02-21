efe8bcbe796cbe7ae4bc562656b935411b9845c4
azu <azuciao@gmail.com>
2014-02-21 10:06:32 +0900

    update script

--- tools/git-sync.sh
+++ tools/git-sync.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 declare parentDir=$(cd $(dirname $(cd $(dirname $0);pwd));pwd)
-cd $(cd $(dirname $(cd $(dirname $0);pwd));pwd)
 git fetch && git merge origin/gh-pages
-git add -A .
+git add -A "$parentDir"
 git commit -m "$1"
 git push
 
