688b6b493619c72e9d3c08401e7cc371db7cb768
azu <azuciao@gmail.com>
2014-07-06 11:06:24 +0900

    Add: AngularJS: Novice to Ninja - O'Reilly Media

--- tools/git-sync.sh
+++ tools/git-sync.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 declare parentDir=$(cd $(dirname $(cd $(dirname $0);pwd));pwd)
 cd $(cd $(dirname $(cd $(dirname $0);pwd));pwd)
-git fetch && git merge origin/gh-pages
+git fetch
+git merge origin/gh-pages
 git add -A .
 git commit -m "$1"
 git push
-
