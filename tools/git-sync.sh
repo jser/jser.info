98cf416abdc65c3b11196d77bbec453848ca0ba7
azu <azuciao@gmail.com>
2015-02-04 22:11:05 +0900

    ES6時代のJavaScript - クックパッド開発者ブログ

--- tools/git-sync.sh
+++ tools/git-sync.sh
@@ -3,7 +3,9 @@
 set -e
 
 parentDir=$(cd $(dirname $(cd $(dirname $0);pwd));pwd)/.git/
+git stash
 git --git-dir="${parentDir}" pull
+git stash pop
 git --git-dir="${parentDir}" add -A .
 git --git-dir="${parentDir}" commit -m "$1"
 git --git-dir="${parentDir}" push
