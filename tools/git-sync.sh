dc0012bfa2e76fc252aaa05f9ee577b7a5585e52
azu <azuciao@gmail.com>
2015-02-06 00:39:16 +0900

    update script

--- tools/git-sync.sh
+++ tools/git-sync.sh
@@ -2,21 +2,22 @@
 
 set -e
 commitMessage=$1
-parentDir=$(cd $(dirname $(cd $(dirname $0);pwd));pwd)/.git/
-echo "${parentDir}"
-isClean=$(git --git-dir="${parentDir}" status --porcelain)
+parentDir=$(cd $(dirname $(cd $(dirname $0);pwd));pwd)
+cd "${parentDir}"
+
+isClean=$(git status --porcelain)
 if [ -z "${isClean}" ]; then
   # Working directory clean
-  git --git-dir="${parentDir}" pull
-  git --git-dir="${parentDir}" push
+  git pull
+  git push
 else
   # Uncommitted changes
-  git --git-dir="${parentDir}" stash
-  git --git-dir="${parentDir}" pull
-  git --git-dir="${parentDir}" stash pop
+  git stash
+  git pull
+  git stash pop
   if [ -n "${commitMessage}" ]; then
-    git --git-dir="${parentDir}" add -A .
-    git --git-dir="${parentDir}" commit -m "${commitMessage}"
+    git add -A .
+    git commit -m "${commitMessage}"
   fi
-  git --git-dir="${parentDir}" push
+  git push
 fi
