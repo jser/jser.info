d9ac75f15ef8793066a962fce745c6a1ab81b9ab
azu <azuciao@gmail.com>
2015-06-13 21:17:56 +0900

    fix(tool): ファイルパスを実行依存にしないように

--- tools/converter/linkCounter.js
+++ tools/converter/linkCounter.js
@@ -3,33 +3,33 @@
  * LICENSE : MIT
  */
 "use strict";
-var pather = require("path");
+var path = require("path");
 var FS = require("q-io/fs");
 var Promise = require("bluebird");
 
 function pLoadFileList(fileList) {
     return Promise.all(fileList.map(function (filePath) {
-        return FS.read(pather.resolve(filePath))
+        return FS.read(path.resolve(filePath))
             .then(JSON.parse)
     }));
 }
-var concatenateJSONPromise = FS.listTree("../../data/", function isIndexHTML(filePath, stat) {
+var concatenateJSONPromise = FS.listTree(path.join(__dirname, "../../data/"), function isIndexHTML(filePath, stat) {
     if (stat.isDirectory()) {
         return false;
     }
-    return pather.basename(filePath) === "index.json";
+    return path.basename(filePath) === "index.json";
 }).then(function (fileList) {
     return Promise.all(fileList.map(function (filePath) {
-        return FS.read(pather.resolve(filePath))
+        return FS.read(path.resolve(filePath))
             .then(JSON.parse)
             .then(function (object) {
                 var obj = {};
-                var fileName = pather.dirname(filePath);
+                var fileName = path.dirname(filePath);
                 var match = fileName.match(/\/(\d{4})\/(\d{2})$/);
                 var fileAttr = {
                     year: match[1],
                     month: match[2],
-                    name: pather.basename(filePath)
+                    name: path.basename(filePath)
                 };
                 obj[fileAttr.year + "/" + fileAttr.month] = object["list"];
                 return obj;
@@ -90,18 +90,18 @@ function countURLAttrOfData(data) {
 }
 
 
-function isReleaseTag(tags){
-    if(!tags) {
+function isReleaseTag(tags) {
+    if (!tags) {
         return false;
     }
     return tags.indexOf("ReleaseNote") !== -1;
 }
-function countReleaseNotGitHub(data){
+function countReleaseNotGitHub(data) {
     var results = data.map(function (yearObject) {
         var key = Object.keys(yearObject)[0];
         var obj = {};
         var sitesOfYear = yearObject[key];
-        obj[key] = sitesOfYear.filter(function(site){
+        obj[key] = sitesOfYear.filter(function (site) {
             return isReleaseTag(site.tags) && !/github.com\//.test(site.url);
         }).length;
         return obj;
@@ -112,12 +112,12 @@ function countReleaseNotGitHub(data){
         console.log(key + "\t" + object[key]);
     });
 }
-function countGitHubRelease(data){
+function countGitHubRelease(data) {
     var results = data.map(function (yearObject) {
         var key = Object.keys(yearObject)[0];
         var obj = {};
         var sitesOfYear = yearObject[key];
-        obj[key] = sitesOfYear.filter(function(site){
+        obj[key] = sitesOfYear.filter(function (site) {
             return isReleaseTag(site.tags) && /github.com\//.test(site.url);
         }).length;
         return obj;
