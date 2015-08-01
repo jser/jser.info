37f7d89a76361d5257e4fd6163bb1f022dccfd59
azu <azuciao@gmail.com>
2015-08-02 01:13:43 +0900

    chore: no force update

--- tools/update-realtime.sh
+++ tools/update-realtime.sh
@@ -21,9 +21,9 @@ npm run build:posts
 git add .
 git commit -m "${lastCommit}"
 if [ -z "${GH_TOKEN}" ]; then
-    git push --force --quiet "https://github.com/jser/realtime.jser.info.git" gh-pages:gh-pages > /dev/null
+    git push --quiet "https://github.com/jser/realtime.jser.info.git" gh-pages:gh-pages > /dev/null
 else
-    git push --force --quiet "https://${GH_TOKEN}@github.com/jser/realtime.jser.info.git" gh-pages:gh-pages > /dev/null
+    git push --quiet "https://${GH_TOKEN}@github.com/jser/realtime.jser.info.git" gh-pages:gh-pages > /dev/null
 fi
 # pop
 cd -
