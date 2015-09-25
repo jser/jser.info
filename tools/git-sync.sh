9075c821b67e2da55b9eb60a7ebfc57dc403fa5a
azu <azuciao@gmail.com>
2015-09-25 15:06:13 +0900

    update tools

--- tools/git-sync.sh
+++ tools/git-sync.sh
@@ -1,9 +1,21 @@
 ﻿#!/bin/sh
 
-set -e
 commitMessage=$1
+currentDir=$(cd $(dirname $0) && pwd)
 parentDir=$(cd $(dirname $(cd $(dirname $0);pwd));pwd)
+
+# http://d.hatena.ne.jp/akuwano/20120411/1334113556
+# Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
+exec > >(tee ${currentDir}/logfile.log)
+
+# Without this, only stdout would be captured - i.e. your
+# log file would not contain any error messages.
+# SEE answer by Adam Spiers, which keeps STDERR a seperate stream -
+# I did not want to steal from him by simply adding his answer to mine.
+exec 2>&1
+
 cd "${parentDir}"
+
 
 isClean=$(git status --porcelain)
 if [ -z "${isClean}" ]; then
@@ -27,3 +39,7 @@ if [[ -z $(git status --porcelain) ]];then
 else
   type /usr/local/bin/terminal-notifier >/dev/null 2>&1 && /usr/local/bin/terminal-notifier -message "Sync!!" -title "JSer.info"
 fi
+
+
+# 退避していたFDをもとに戻す
+exec 1>&3
