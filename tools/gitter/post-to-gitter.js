b6ede669838b679ff4f87743722567401b73e5f4
azu <azuciao@gmail.com>
2016-02-22 00:01:26 +0900

    chore(gitter): change chat id

--- tools/gitter/post-to-gitter.js
+++ tools/gitter/post-to-gitter.js
@@ -6,7 +6,8 @@ module.exports = function postToGitter(message) {
     if (!token) {
         return Promise.reject(new Error("Have to set GITTER_TOKEN env."));
     }
-    return fetch("https://api.gitter.im/v1/rooms/56c9cae2e610378809c29b86/chatMessages", {
+    // jser.info 53d32e61107e137846ba8031
+    return fetch("https://api.gitter.im/v1/rooms/53d32e61107e137846ba8031/chatMessages", {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
