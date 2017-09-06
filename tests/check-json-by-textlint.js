c497f0b6833a33b10de2f14307156a63974eec8b
azu <azuciao@gmail.com>
2017-09-06 10:18:06 +0900

    use proofdict insteadof spellcheck-tech-word

--- tests/check-json-by-textlint.js
+++ tests/check-json-by-textlint.js
@@ -1,8 +1,8 @@
 // LICENSE : MIT
 "use strict";
 // 最新のJSONのみを対象に技術用語のスペルチェックを動かす
-var lintTextContent = require("./lint-text-content");
-var latestDataPath = require("./lib/data-manager").getLatestData(new Date());
+const lintTextContent = require("./lint-text-content");
+const latestDataPath = require("./lib/data-manager").getLatestData(new Date());
 lintTextContent(latestDataPath).then(function () {
     process.exit(0);
 }).catch(function (error) {
