2425e2c6f7b963dd90af59b180062cf03082c9d4
azu <azuciao@gmail.com>
2018-03-18 13:33:07 +0900

    fix: update proofdict

--- tests/lint-text-content.js
+++ tests/lint-text-content.js
@@ -1,16 +1,19 @@
 // LICENSE : MIT
 "use strict";
-var prettyError = require("textlint-formatter/lib/formatters/pretty-error").prettyError;
-var path = require("path");
+const { prettyError } = require("@textlint/linter-formatter/lib/linter-formatter/src/formatters/pretty-error");
+const path = require("path");
+
 function hasError(result) {
     return result.messages.length > 0;
 }
+
 function printResult(data, filePath, message) {
-    var output = prettyError(data["content"], filePath, message);
+    const output = prettyError(data["content"], filePath, message);
     console.log(output);
     console.log(JSON.stringify(data, null, 4));
 
 }
+
 function lint(filePath) {
     if (!filePath) {
         return Promise.reject("Error: filePath is not found.");
@@ -21,20 +24,20 @@ function lint(filePath) {
         configFile: path.join(__dirname, ".textlintrc")
     });
     var list = json.list;
-    var promises = list.map(function (item) {
+    var promises = list.map(function(item) {
         var content = item.content;
         return engine.executeOnText(content, ".md").then(results => results[0]);
     });
-    return Promise.all(promises).then(function (results) {
+    return Promise.all(promises).then(function(results) {
         var isError = false;
-        results.forEach(function (result, index) {
+        results.forEach(function(result, index) {
             if (!hasError(result)) {
                 return;
             }
             isError = true;
             // エラーがある
             var originalData = list[index];
-            result.messages.forEach(function (message) {
+            result.messages.forEach(function(message) {
                 printResult(originalData, filePath, message);
             });
         });
